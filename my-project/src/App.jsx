6b17cfb221014d9e1797e0ed4fa26a5ac3ef138d
MartinSimov <martin.simov94@gmail.com>
2023-11-16 18:50:44 +0200

    Setup: NavBar,LogIn, SignIn  ROUTES

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import LogIn from './components/LogIn';
+import NavBar from './components/NavBar';
+import SignIn from './components/SignIn';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    
-      <div>
-       <button className="btn btn-accent">Accent</button>
-      </div>
-    
-    
-  )
+    <div className="App">
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/LogIn" element={<LogIn />} />
+        <Route path="/SignIn" element={<SignIn />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ my-project/src/components/NavBar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <div className="navbar bg-gray-700">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl text-white">Team 8 Infinity</a>
+  </div>
+  <div className="flex-none gap-2">
+    <div className="dropdown dropdown-end">
+      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img alt="Tailwind CSS Navbar component" src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </label>
+      <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><Link to="/LogIn">Log In</Link></li>
+        <li><Link to="/SignIn">Sign In</Link></li>
+      </ul>
+    </div>
+  </div>
+</div>
+  )
+}
+
+export default NavBar;
