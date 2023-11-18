fe51ff4d5e83317954dd758eefe6918ac0398d97
BorisMil <b.milanovv@abv.bg>
2023-11-18 16:26:23 +0200

    Add SignUp-validation

--- my-project/src/components/SignUp.jsx
+++ my-project/src/components/SignUp.jsx
@@ -1,28 +1,37 @@
-import React from 'react';
-import{ useState } from 'react';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../config/firebase-config';
-import { getDatabase, ref, set } from 'firebase/database';
-import { createUser } from '../services/user.sevices';
+import { useFormik } from "formik";
+import { basicSchema } from '../schemas/index.js'; 
 
 const SignUp = () => {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [phoneNumber, setNumber] = useState('');
-  const [password, setPassword] = useState('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-      const user = userCredential.user;
-      const db = getDatabase();
-      console.log(user);
-      await createUser(username, email, phoneNumber, user.uid)
-    } catch (error) {
-      console.error("Registration error:", error.message);
-    }
-  };
+  const {values, handleBlur, handleChange, handleSubmit, errors, touched} = useFormik({
+    initialValues:{
+      username: '',
+      email: '',
+      phoneNumber: '',
+      password: '',
+      confirmPassword: '',
+    },
+    validationSchema: basicSchema,
+  })
+
+  // const [username, setUsername] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [phoneNumber, setNumber] = useState('');
+  // const [password, setPassword] = useState('');
+  // const [confirmPassword, setConfirmPassword] = useState('');
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+  //     const user = userCredential.user;
+  //     const db = getDatabase();
+  //     console.log(user);
+  //     await createUser(username, email, phoneNumber, user.uid)
+  //   } catch (error) {
+  //     console.error("Registration error:", error.message);
+  //   }
+  // };
 
   return (
     <div className="hero min-h-screen bg-base-200">
@@ -37,29 +46,42 @@ const SignUp = () => {
               <label className="label">
                 <span className="label-text">Username</span>
               </label>
-              <input type="text" placeholder="username" className="input input-bordered" 
-                value={username} onChange={(e) => setUsername(e.target.value)} required />
+              <input type="text" placeholder="Type Your Username" className="input input-bordered" 
+                value={values.username} onChange={handleChange} onBlur={handleBlur} id='username'  required />
+                {touched.username && errors.username && <p className="text-red-500">{errors.username}</p>}
             </div>
+
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
-              <input type="email" placeholder="email" className="input input-bordered" 
-                value={email} onChange={(e) => setEmail(e.target.value)} required />
+              <input type="email" placeholder="Type Your Email" className="input input-bordered" 
+                value={values.email} onChange={handleChange} onBlur={handleBlur} id='email' required />
+                {touched.email && errors.email && <p className="text-red-500">{errors.email}</p>}
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Phone Number</span>
               </label>
-              <input type="text" placeholder="number" className="input input-bordered" 
-                value={phoneNumber} onChange={(e) => setNumber(e.target.value)} required />
+              <input type="text" placeholder="Type Your Phone Number" className="input input-bordered" 
+                value={values.phoneNumber} onChange={handleChange} onBlur={handleBlur} id='phoneNumber' required />
+                {touched.phoneNumber && errors.phoneNumber && <p className="text-red-500">{errors.phoneNumber}</p>}
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input type="password" placeholder="password" className="input input-bordered" 
-                value={password} onChange={(e) => setPassword(e.target.value)} required />
+              <input type="password" placeholder="Type Your Password" className="input input-bordered" 
+                value={values.password} onChange={handleChange} onBlur={handleBlur} id='password'  required />
+                {touched.password && errors.password && <p className="text-red-500">{errors.password}</p>}
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Confirm Password</span>
+              </label>
+              <input type="password" placeholder="Confirm Your Password" className="input input-bordered" 
+                value={values.confirmPassword} onChange={handleChange} onBlur={handleBlur}id='confirmPassword' required />
+                 {touched.confirmPassword && errors.confirmPassword && <p className="text-red-500">{errors.confirmPassword}</p>}
             </div>
             <div className="form-control mt-6">
               <button className="btn btn-primary">Register</button>
---
+++ my-project/src/schemas/index.js
@@ -0,0 +1,12 @@
+import * as yup from "yup";
+
+const passwordRules = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z]).{6,20}$/;
+
+
+export const basicSchema = yup.object().shape({
+    username: yup.string().min(3,'At least 4 symbols').max(30,'Max 30 symbols').required("Username is required"),
+    email: yup.string().required("Email is required"),
+    phoneNumber: yup.string().required("Phone Number is required").matches(/^\d{10}$/, 'Phone number must have exactly 10 digits'),
+    password: yup.string().required("Password is required").matches(passwordRules,'Password must be 6-20 characters with at least 1 uppercase, 1 number, and 1 symbol. '),
+    confirmPassword: yup.string().oneOf([yup.ref("password"), null], "Passwords must match"),
+})
