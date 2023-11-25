5a02ca261d24abae5e616bed792b9d56eec1c4c6
BorisMil <b.milanovv@abv.bg>
2023-11-26 01:45:19 +0200

    Add categories on PrivateHome-User and add text Home.jsx

--- my-project/src/components/Home.jsx
+++ my-project/src/components/Home.jsx
@@ -3,11 +3,14 @@ const Home = () => {
     <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row">
     <img src="https://picsum.photos/200/300" className="max-w-sm rounded-lg shadow-2xl" />
-    <div>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
-    </div>
+    <div className="flex flex-col items-center justify-center ">
+  <h1 className="text-5xl font-bold">Welcome to AIQuiz!</h1>
+  <p className="py-6 text-8xs text-center">
+    Unlock the world of knowledge and fun with AIQuiz, your go-to platform for creating, sharing, and participating in quizzes. Explore a diverse range of quizzes crafted by educators, enthusiasts, and experts. From science and technology to literature and entertainment, there's a quiz for everyone.
+  </p>
+  <button onClick={() => window.location.href = '/SignUp'} className="btn btn-primary">Get Started</button>
+</div>
+
   </div>
 </div>
   );
--- my-project/src/components/PrivateHome/PrivateHome.jsx
+++ my-project/src/components/PrivateHome/PrivateHome.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { getAllQuizzes } from '../../services/quiz.services';
 
+
 const PrivateHome = () => {
     const [quizzes, setQuizzes] = useState();
     useEffect(()=>{
@@ -10,7 +11,72 @@ const PrivateHome = () => {
         console.log(quizzes);
     },[quizzes])
   return (
-    <div>PrivateHome</div>
+  <div className='flex '>
+    <div className="card w-64 h-50 bg-neutral text-neutral-content">
+  <div className="card-body items-center text-center">
+    <h2 className="card-title"><div className="badge badge-primary badge-lg"></div>
+<div className="badge badge-primary badge-md"></div>
+<div className="badge badge-primary badge-sm"></div>
+<div className="badge badge-primary badge-xs"></div>
+</h2>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary text-2xl ">Books</button>
+    </div>
+    <p className='text-red-500' >Try It Out</p>
+  </div>
+</div>
+<div className="card w-64 h-50 bg-neutral text-neutral-content">
+  <div className="card-body items-center text-center">
+    <h2 className="card-title"><div className="badge badge-primary badge-lg"></div>
+<div className="badge badge-primary badge-md"></div>
+<div className="badge badge-primary badge-sm"></div>
+<div className="badge badge-primary badge-xs"></div>
+</h2>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary text-2xl ">Books</button>
+    </div>
+  </div>
+</div>
+
+<div className="card w-64 h-50 bg-neutral text-neutral-content">
+  <div className="card-body items-center text-center">
+    <h2 className="card-title"><div className="badge badge-primary badge-lg"></div>
+<div className="badge badge-primary badge-md"></div>
+<div className="badge badge-primary badge-sm"></div>
+<div className="badge badge-primary badge-xs"></div>
+</h2>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary text-2xl ">Books</button>
+    </div>
+  </div>
+</div>
+
+<div className="card w-64 h-50 bg-neutral text-neutral-content">
+  <div className="card-body items-center text-center">
+    <h2 className="card-title"><div className="badge badge-primary badge-lg"></div>
+<div className="badge badge-primary badge-md"></div>
+<div className="badge badge-primary badge-sm"></div>
+<div className="badge badge-primary badge-xs"></div>
+</h2>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary text-2xl ">Books</button>
+    </div>
+  </div>
+</div>
+
+<div className="card w-64 h-50 bg-neutral text-neutral-content">
+  <div className="card-body items-center text-center">
+    <h2 className="card-title"><div className="badge badge-primary badge-lg"></div>
+<div className="badge badge-primary badge-md"></div>
+<div className="badge badge-primary badge-sm"></div>
+<div className="badge badge-primary badge-xs"></div>
+</h2>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary text-2xl ">Books</button>
+    </div>
+  </div>
+</div>
+</div>
   )
 }
 
