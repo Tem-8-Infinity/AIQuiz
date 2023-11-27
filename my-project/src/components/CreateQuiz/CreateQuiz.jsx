9ceb95d6bb553361cab14ce8d0e7e2f7f2ece578
MartinSimov <martin.simov94@gmail.com>
2023-11-27 14:14:52 +0200

    Clicked button in color

--- my-project/src/components/CreateQuiz/CreateQuiz.jsx
+++ my-project/src/components/CreateQuiz/CreateQuiz.jsx
@@ -16,6 +16,13 @@ const CreateQuiz = () => {
     }));
   };
 
+  const handlePrivacyChange = (isPrivate) => {
+    setQuizDetails(prevDetails => ({
+      ...prevDetails,
+      isPrivate: isPrivate
+    }));
+  };
+
   const handleCreateQuiz = () => {
     // Implement the logic to create a quiz using quizDetails
     console.log('Quiz Created:', quizDetails);
@@ -26,69 +33,70 @@ const CreateQuiz = () => {
   useEffect(()=>{
     console.log(quizDetails);
   },[quizDetails])
+  
   return (
     <div>
-
       <div className="card w-96 bg-base-100 shadow-xl">
         <div className="card-body">
-          {/* Content */}
           <h1>Create Quiz</h1>
           <div>
             <label>Category :</label>
-            <select name='category' value={quizDetails.category}
+            <select 
+              name='category' 
+              value={quizDetails.category}
               onChange={handleInputChange}
-             className="select select-bordered w-full max-w-xs">
-              
-              {/* <option>Books</option>
-              <option>Films</option>
-              <option>Animals</option>
-              <option>History</option>
-               */}
-               {categories.map((category, index) => { 
-               if(index === 0){
-                return(
-                  <option disabled key={category}>{category}</option> 
-                )
-              }
-              return(
-                <option key={category}>{category}</option>
-              )
-              }
-               )}
+              className="select select-bordered w-full max-w-xs"
+            >
+              {categories.map((category, index) => (
+                <option key={category} disabled={index === 0}>
+                  {category}
+                </option>
+              ))}
             </select>
-      
           </div>
           <div>
             <label>Title :</label>
-            <input value={quizDetails.title}
-              onChange={handleInputChange} name='title'
-              type="text" placeholder="Title of the quiz" className="input input-bordered w-full max-w-xs" />
+            <input 
+              value={quizDetails.title}
+              onChange={handleInputChange} 
+              name='title'
+              type="text" 
+              placeholder="Title of the quiz" 
+              className="input input-bordered w-full max-w-xs" 
+            />
           </div>
           <div>
             <label>Timer :</label>
-            <input value={quizDetails.title}
-              onChange={handleInputChange} name='title'
-              type="text" placeholder="Set quiz timer" className="input input-bordered w-full max-w-xs" />
+            <input 
+              value={quizDetails.timer}
+              onChange={handleInputChange} 
+              name='timer'
+              type="number" 
+              placeholder="Set quiz timer" 
+              className="input input-bordered w-full max-w-xs" 
+            />
           </div>
           <div>
-          <button className="btn btn-outline">Open</button>
-          <button className="btn btn-outline">Private</button>
-            {/* <label>Private:</label>
-            <input
-              type="checkbox"
-              checked={quizDetails.isPrivate}
-              onChange={() =>
-                handleInputChange('isPrivate', !quizDetails.isPrivate)
-              }
-            /> */}
+            <button 
+              className={`btn ${quizDetails.isPrivate ? 'btn-outline' : 'btn-primary'}`} 
+              onClick={() => handlePrivacyChange(false)}
+            >
+              Open
+            </button>
+            <button 
+              className={`btn ${quizDetails.isPrivate ? 'btn-primary' : 'btn-outline'}`}
+              onClick={() => handlePrivacyChange(true)}
+            >
+              Private
+            </button>
           </div>
           <div>
-            <button className='btn btn-primary' onClick={handleCreateQuiz}>Create Questionnaire</button >
+            <button className='btn btn-primary' onClick={handleCreateQuiz}>Create Questionnaire</button>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default CreateQuiz
+export default CreateQuiz;
