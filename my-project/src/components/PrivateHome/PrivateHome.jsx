f478605d5450c492cdfb4add65cdc74104427e51
BorisMil <b.milanovv@abv.bg>
2023-11-27 12:49:03 +0200

    Add categories

--- my-project/src/components/PrivateHome/PrivateHome.jsx
+++ my-project/src/components/PrivateHome/PrivateHome.jsx
@@ -11,7 +11,7 @@ const PrivateHome = () => {
         console.log(quizzes);
     },[quizzes])
   return (
-  <div className='flex '>
+  <div className='flex flex-gap '>
     <div className="card w-64 h-50 bg-neutral text-neutral-content">
   <div className="card-body items-center text-center">
     <h2 className="card-title"><div className="badge badge-primary badge-lg"></div>
@@ -20,7 +20,7 @@ const PrivateHome = () => {
 <div className="badge badge-primary badge-xs"></div>
 </h2>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary text-2xl ">Books</button>
+    <a href="/UserQuizPreview"> <button className="btn btn-primary text-2xl ">Books</button></a>
     </div>
     <p className='text-red-500' >Try It Out</p>
   </div>
@@ -33,7 +33,7 @@ const PrivateHome = () => {
 <div className="badge badge-primary badge-xs"></div>
 </h2>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary text-2xl ">Books</button>
+      <button className="btn btn-primary text-2xl ">Bikes</button>
     </div>
   </div>
 </div>
@@ -46,7 +46,7 @@ const PrivateHome = () => {
 <div className="badge badge-primary badge-xs"></div>
 </h2>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary text-2xl ">Books</button>
+      <button className="btn btn-primary text-2xl ">History</button>
     </div>
   </div>
 </div>
@@ -59,7 +59,7 @@ const PrivateHome = () => {
 <div className="badge badge-primary badge-xs"></div>
 </h2>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary text-2xl ">Books</button>
+      <button className="btn btn-primary text-2xl ">Science</button>
     </div>
   </div>
 </div>
@@ -72,7 +72,7 @@ const PrivateHome = () => {
 <div className="badge badge-primary badge-xs"></div>
 </h2>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary text-2xl ">Books</button>
+      <button className="btn btn-primary text-2xl ">Animals</button>
     </div>
   </div>
 </div>
---
+++ my-project/src/components/UserQuizPreview/UserQuizPreview.jsx
@@ -0,0 +1,11 @@
+
+const UserQuizPreview = () => {
+
+    return (
+      <div>
+        UserQuizPreview 
+      </div>
+    );
+  };
+  
+  export default UserQuizPreview;
