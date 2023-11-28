e83f7bccb2ccd5c09ffda27680c53fe2dc2c141a
BorisMil <b.milanovv@abv.bg>
2023-11-28 19:41:06 +0200

    Add CreateQuiz page info to store in the firebase and made new page CeateQuestionnare

---
+++ my-project/src/services/CreateQuestionnaire/CreateQuestionnaire.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CreateQuestionnaire = () => {
+    const [quizQuestionnaire, setQuizQuestionnaire] = useState({
+
+      });
+  return (
+    <div>CreateQuestionnaire</div>
+  )
+}
+
+export default CreateQuestionnaire
